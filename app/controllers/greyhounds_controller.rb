d7466b17befeec86efa0151103912282da4628ab
Jez Nicholson <jez.nicholson@gmail.com>
2021-05-10 18:09:55 +0100

    use sqlite locally

--- app/controllers/greyhounds_controller.rb
+++ app/controllers/greyhounds_controller.rb
@@ -1,15 +1,14 @@
 class GreyhoundsController < ApplicationController
-
   def index
     @greyhounds = Greyhound.all
     respond_to do |format|
       format.html
       format.docx do
-        @volume = "1"
-        @issue = "1"
+        @volume = '1'
+        @issue = '1'
         template = Tapir::Reports::Template.new('app/assets/images/kennel_times.docx')
         s = template.output(binding, [])
-        send_data s, filename: "kennel_times.docx", type: :docx, disposition: :inline
+        send_data s, filename: 'kennel_times.docx', type: :docx, disposition: :inline
       end
     end
   end
@@ -20,14 +19,11 @@ class GreyhoundsController < ApplicationController
       format.html
       format.docx do
         template = Tapir::Reports::Template.new('app/assets/images/profile.docx')
-        replacements =
-          [
-            ['photo_uri', @greyhound.photo_uri]
-          ]
+        replacements = []
+        replacements << ['photo_uri', @greyhound.photo_uri]
         s = template.output(binding, replacements)
         send_data s, filename: "#{@greyhound.name}_profile.docx", type: :docx, disposition: :inline
       end
     end
   end
-
 end
